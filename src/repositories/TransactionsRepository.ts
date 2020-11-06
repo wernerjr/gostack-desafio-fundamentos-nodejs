dc9acd09fcb97fcef0472be66220589b2fe089fe
Werner Gehrke Junior <werner.junior@outlook.com>
2020-11-05 21:03:30 -0300

    Resolução do desafio 5

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,16 +20,44 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.getTypeValue('income');
+    const outcome = this.getTypeValue('outcome');
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  private getTypeValue(type: 'income' | 'outcome'): number {
+    return this.transactions.reduce(
+      (total, transaction) => this.getTypeSum(total, transaction, type),
+      0,
+    );
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private getTypeSum(
+    total: number,
+    transaction: Transaction,
+    type: 'income' | 'outcome',
+  ): number {
+    return type === transaction.type ? total + transaction.value : total;
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
-
 export default TransactionsRepository;
